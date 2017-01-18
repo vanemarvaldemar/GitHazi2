04a34870e5dca467e443d7e5421f5ca6e95312e4
karoly.vass <karoly.vass@semilab.hu>
2017-01-18 15:25:54 +0100

    Engine implemented

--- GitHazi2/GitHazi2/Program.cs
+++ GitHazi2/GitHazi2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 
 namespace GitHazi2
@@ -16,12 +17,62 @@ namespace GitHazi2
 
         internal class Engine
         {
+            private readonly IAdatEllenorzo _adatellEnorzo;
+
+            bool fuss = true;
+            StringBuilder buffer = new StringBuilder();
+
             public Engine(IAdatEllenorzo adatEllenorzo)
             {
+                _adatellEnorzo = adatEllenorzo;
             }
 
             public void Start(int kitalalandoSzam)
             {
+                Console.WriteLine("Gondoltam egy számra 1 és 1000 között. Tippelj!");
+                while (fuss)
+                {
+                    var key = Console.ReadKey();
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            fuss = false;
+                            break;
+                        case ConsoleKey.Enter:
+                            string tippString = buffer.ToString();
+                            int tipp;
+                            buffer.Clear();
+                            Console.Clear();
+                            if (!_adatellEnorzo.ErvenyesE(tippString, out tipp))
+                            {
+                                Console.WriteLine("Érvénytelen szám!");
+                            }
+                            else
+                            {
+                                switch (_adatellEnorzo.TalaltE(tipp))
+                                {
+                                    case TippEllenorzesEredmeny.Talalt:
+                                        Console.WriteLine("Talált!");
+                                        fuss = false;
+                                        break;
+                                    case TippEllenorzesEredmeny.Ala:
+                                        Console.WriteLine("Alá!");
+                                        break;
+                                    case TippEllenorzesEredmeny.Fole:
+                                        Console.WriteLine("Fölé!");
+                                        break;
+                                }
+                            }
+
+                            break;
+                        default:
+                            buffer.Append(key.KeyChar);
+                            break;
+                    }
+                }
+
+                Console.WriteLine("Vége. Nyomj gombot.");
+                Console.ReadKey();
             }
         }
         }
