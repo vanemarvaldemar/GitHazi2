6ce5f323734b00cd381e1edf915bc9d5ea8ca83e
Peter Nagy <djpetko@gmail.com>
2017-01-18 15:55:36 +0100

    func2

--- GitHazi2/GitHazi2/Adatellenorzo.cs
+++ GitHazi2/GitHazi2/Adatellenorzo.cs
@@ -6,12 +6,32 @@ namespace GitHazi2
     {
         public bool ErvenyesE(string inputValue, out int tipp)
         {
-            throw new NotImplementedException();
+
+            if (!int.TryParse(inputValue, out tipp))
+            {
+                Console.WriteLine("Nem szám!");
+                return false;
+            }
+            if (tipp < 0 || tipp > 1000)
+            {
+                Console.WriteLine("Mondom 1 és 1000 között!");
+                return false;
+            }
+            return true;
         }
 
-        public TippEllenorzesEredmeny TalaltE(int kitalalandoSzam, int tipp)
+        public TippEllenorzesEredmeny TalaltE(int kitalalando, int tipp)
         {
-            throw new NotImplementedException();
+            switch (Math.Sign(tipp - kitalalando))
+            {
+                case -1:
+                    return TippEllenorzesEredmeny.Ala;
+                case 1:
+                    return TippEllenorzesEredmeny.Fole;
+                case 0:
+                    return TippEllenorzesEredmeny.Talalt;
+            }
+            return TippEllenorzesEredmeny.Ala;
         }
     }
 }
