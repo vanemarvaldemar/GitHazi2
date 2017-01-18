ee90cdf48f68e96e0051de638dbdd08de7a5486b
karoly.vass <karoly.vass@semilab.hu>
2017-01-18 15:46:20 +0100

    done

--- GitHazi2/GitHazi2/Program.cs
+++ GitHazi2/GitHazi2/Program.cs
@@ -11,7 +11,8 @@ namespace GitHazi2
             var random = new Random(DateTime.Now.Millisecond);
             var szam = random.Next(0, 1000);
 
-            var engine = new Engine(new Adatellenorzo());
+//            var engine = new Engine(new Adatellenorzo());
+            var engine = new Engine(null);
             engine.Start(szam);
         }
 
@@ -77,37 +78,38 @@ namespace GitHazi2
         }
         }
 
-    internal class Adatellenorzo : IAdatEllenorzo
-    {
+    //Test implementation
+    //internal class Adatellenorzo : IAdatEllenorzo
+    //{
 
-        public bool ErvenyesE(string inputValue, out int tipp)
-        {
+    //    public bool ErvenyesE(string inputValue, out int tipp)
+    //    {
  
-            if (!int.TryParse(inputValue, out tipp))
-            {
-                Console.WriteLine("Nem szám!");
-                return false;
-            }
-            if (tipp < 0 || tipp > 1000)
-            {
-                Console.WriteLine("Mondom 1 és 1000 között!");
-                return false;
-            }
-            return true;
-        }
+    //        if (!int.TryParse(inputValue, out tipp))
+    //        {
+    //            Console.WriteLine("Nem szám!");
+    //            return false;
+    //        }
+    //        if (tipp < 0 || tipp > 1000)
+    //        {
+    //            Console.WriteLine("Mondom 1 és 1000 között!");
+    //            return false;
+    //        }
+    //        return true;
+    //    }
 
-        public TippEllenorzesEredmeny TalaltE(int kitalalando, int tipp)
-        {
-            switch (Math.Sign(tipp - kitalalando))
-            {
-                case -1:
-                    return TippEllenorzesEredmeny.Ala;
-                case 1:
-                    return TippEllenorzesEredmeny.Fole;
-                case 0:
-                    return TippEllenorzesEredmeny.Talalt;
-            }
-            return TippEllenorzesEredmeny.Ala;
-        }
-    }
+    //    public TippEllenorzesEredmeny TalaltE(int kitalalando, int tipp)
+    //    {
+    //        switch (Math.Sign(tipp - kitalalando))
+    //        {
+    //            case -1:
+    //                return TippEllenorzesEredmeny.Ala;
+    //            case 1:
+    //                return TippEllenorzesEredmeny.Fole;
+    //            case 0:
+    //                return TippEllenorzesEredmeny.Talalt;
+    //        }
+    //        return TippEllenorzesEredmeny.Ala;
+    //    }
+    //}
 }
