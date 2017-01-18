d4d5302f446123f43e51759d806520b3f152862e
Peter Nagy <djpetko@gmail.com>
2017-01-18 15:55:08 +0100

    func1

---
+++ GitHazi2/GitHazi2/Adatellenorzo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitHazi2
+{
+    public class Adatellenorzo : IAdatEllenorzo
+    {
+        public bool ErvenyesE(string inputValue, out int tipp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TippEllenorzesEredmeny TalaltE(int kitalalandoSzam, int tipp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
